d82da4d7185875f0949e25b7150453119fb3fa41
s5hy25ni <s5hy25ni@gmail.com>
2023-06-08 18:43:41 +0900

    mybatis

---
+++ web002_MyBatis_Step01/src/main/java/com/min/edu/vo/JobVo.java
@@ -0,0 +1,21 @@
+package com.min.edu.vo;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class JobVo {
+	// Java.NET Object
+	// ct+st+a -> 소문자
+	private String job_id;
+	private String job_title;
+	private int min_salary;
+	private int max_salary;
+}
---
+++ web002_MyBatis_Step01/src/test/java/com/test/edu/JUnitTest_Select.java
@@ -0,0 +1,16 @@
+package com.test.edu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.min.edu.vo.JobVo;
+
+public class JUnitTest_Select {
+
+	@Test
+	public void test() {
+		JobVo vo = new JobVo();
+	}
+
+}
